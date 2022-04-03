586f9f20269316e594f4e59578a03e730623f60f
Reza Farjam <reza.farjam78@gmail.com>
2022-04-04 03:05:10 +0430

    Switched to MySQL

--- cse_archive/settings.py
+++ cse_archive/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from env import DB_PASSWORD, DB_USER
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,8 +86,11 @@ WSGI_APPLICATION = 'cse_archive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cse_archive',
+        'HOST': 'localhost',
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
     }
 }
 
