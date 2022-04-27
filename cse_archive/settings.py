c43e52fa34e66a98b7ea03e677a884484ea886ed
Reza Farjam <reza.farjam78@gmail.com>
2022-04-27 20:06:38 +0430

    Moved media and staticfiles Folders to Root Directoy

--- cse_archive/settings.py
+++ cse_archive/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = [
@@ -152,9 +152,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/').replace('\\', '/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Localization
 
