53270b51b361e8c7f080fcf827da844b3db5d5a5
Reza Farjam <reza.farjam78@gmail.com>
2022-05-20 18:37:51 +0430

    Fixed TypeError

--- teacher/validators.py
+++ teacher/validators.py
@@ -4,5 +4,5 @@ from django.core.exceptions import ValidationError
 def image_size_validator(image):
     max_size = 1
 
-    if image.size > max_size * 1024 * 1024:
+    if int(image.size) > max_size * 1024 * 1024:
         raise ValidationError(f"حجم عکس‌ها نمی‌تواند بیشتر از {max_size} مگابایت باشد.")
