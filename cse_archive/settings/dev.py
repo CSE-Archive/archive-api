8de312c98fcb89a39840dbee6b304fa6190c5d3d
unknown <reza00farjam@gmail.com>
2022-08-06 15:59:11 +0430

    Updated dev settings to accept env vars for DB

--- cse_archive/settings/dev.py
+++ cse_archive/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from .common import *
 
 DEBUG = True
@@ -7,10 +9,10 @@ SECRET_KEY = 'django-insecure-^t!eh64gj_=lh4eatz$rng#^s#n1$!k-(%$s#wtzm9$i0@0(l!
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cse_archive',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
+        'NAME': os.environ.get('DB_NAME', 'cse_archive'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
     }
 }
